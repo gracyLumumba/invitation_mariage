0cb244bfa1e587ff0b17e432de3a13800077c6dd
gracyLumumba <gracylumumba26@gmail.com>
2026-06-04 21:57:22 +0200

    Add invitation physique route for Render deployment

--- invitation 2/server.js
+++ invitation 2/server.js
@@ -348,6 +348,7 @@ app.get('/i/:code', (req, res) => {
   res.redirect(`/?code=${encodeURIComponent(code)}`);
 });
 app.get('/invitation', requireInvite, (req, res) => res.sendFile(path.join(__dirname, 'invitation.html')));
+app.get('/invitation-physique', (req, res) => res.sendFile(path.join(__dirname, 'invitation-physique.html')));
 app.get('/admin', (req, res) => res.sendFile(path.join(__dirname, 'admin.html')));
 app.get('/scanner', (req, res) => res.sendFile(path.join(__dirname, 'scanner.html')));
 app.get('/serveurs', (req, res) => res.sendFile(path.join(__dirname, 'serveurs.html')));
