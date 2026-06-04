f8a0a2771c686fc93e217dbc32bf7a0a0e619245
gracyLumumba <gracylumumba26@gmail.com>
2026-06-04 12:17:35 +0200

    .

--- invitation 2/server.js
+++ invitation 2/server.js
@@ -260,8 +260,7 @@ async function publicInvitationPayload(invite, baseUrl) {
 }
 
 async function nextInviteCode() {
-  for (let i = 1; i < 10000; i += 1) {
-    const code = `TF${String(i).padStart(2, '0')}`;
+  for (let i = 1; i < 10000; i += 1) {    const code = `TL${String(i).padStart(2, '0')}`;
     if (!(await db.codeExists(code))) return code;
   }
   throw new Error('Impossible de générer un code disponible.');
